987560511e2055dddb203c938cc0d96a1c4ebbf6
Thomas Bates <roboturbo2@gmail.com>
2026-02-11 11:34:43 -0700

    Minor improvement to TextUtils.java

--- app/src/main/java/ca/turbobutterfly/core/utils/TextUtils.java
+++ app/src/main/java/ca/turbobutterfly/core/utils/TextUtils.java
@@ -1,20 +1,32 @@
 package ca.turbobutterfly.core.utils;
 
+import java.util.Objects;
+
 public class TextUtils
 {
+    /**
+     * Null-safe check for equality.
+     */
     public static boolean equals(String a, String b)
     {
-        return (a == null) ? (b == null) : a.equals(b);
+        return Objects.equals(a, b);
     }
 
+    /**
+     * Checks if a string is null or empty.
+     */
     public static boolean isEmpty(String a)
     {
         return (a == null) || a.isEmpty();
     }
 
+    /**
+     * Null-safe natural order comparison. 
+     * Treats null as "less than" any non-null string.
+     */
     public static int compare(String a, String b)
     {
-        if ((a == null) & (b == null))
+        if (Objects.equals(a, b))
         {
             return 0;
         }
